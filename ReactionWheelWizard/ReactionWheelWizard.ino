f26f5f3587e51ed862aeb38386e5643e0a476ef8
Cameron Bannasch <cameron.bannasch@gmail.com>
2026-07-11 19:05:35 -0700

    Add simple Vesc communications

--- ReactionWheelWizard/ReactionWheelWizard.ino
+++ ReactionWheelWizard/ReactionWheelWizard.ino
@@ -1,9 +1,20 @@
+#include <VescUart.h>
+
+VescUart uart;
 
 void setup() {
   Serial.begin(9600);
+
+  while (!Serial) {;}
 }
 
 void loop() {
   Serial.println("Hello world!");
+
+  if (uart.getVescValues()) {
+    Serial.printf("RPM: %d", uart.data.rpm);
+  } else {
+    Serial.printf("Failed to get RPM data ):<");
+  }
   delay(1000);
 }
