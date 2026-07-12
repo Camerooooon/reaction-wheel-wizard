3b070858cc412d3e29ca0d2daf8f0fe10b2f57d2
Cameron Bannasch <cameron.bannasch@gmail.com>
2026-07-11 20:29:58 -0700

    Add gyro readout

--- ReactionWheelWizard/ReactionWheelWizard.ino
+++ ReactionWheelWizard/ReactionWheelWizard.ino
@@ -1,5 +1,6 @@
 #include <VescUart.h>
 #include <SoftwareSerial.h>
+#include <Adafruit_BNO055.h>
 
 #define VESC_BAUD_RATE 115200
 #define VESC_RX_PIN 1
@@ -9,6 +10,7 @@
 
 VescUart uart;
 SoftwareSerial vesc_ser = SoftwareSerial(VESC_RX_PIN, VESC_TX_PIN);
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28, &Wire);
 
 void setup() {
   Serial.begin(9600);
@@ -34,6 +36,16 @@ void loop() {
   } else {
     Serial.printf("Could not get fw version\n");
   }
+
+  sensors_event_t orientationData , angVelocityData , linearAccelData, magnetometerData, accelerometerData, gravityData;
+  bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
+  bno.getEvent(&angVelocityData, Adafruit_BNO055::VECTOR_GYROSCOPE);
+  bno.getEvent(&linearAccelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
+  bno.getEvent(&magnetometerData, Adafruit_BNO055::VECTOR_MAGNETOMETER);
+  bno.getEvent(&accelerometerData, Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  bno.getEvent(&gravityData, Adafruit_BNO055::VECTOR_GRAVITY);
+
+  Serial.printf("pos: %d %d %d", orientationData.orientation.x, orientationData.orientation.y, orientationData.orientation.z);
 
   uart.sendKeepalive();
   uart.setCurrent(4);
