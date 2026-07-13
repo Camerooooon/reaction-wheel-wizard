946032be8b2ed7f60f2a7a7000a04e8e5be68733
Cameron Bannasch <cameron.bannasch@gmail.com>
2026-07-12 18:12:08 -0700

    print update

--- ReactionWheelWizard/ReactionWheelWizard.ino
+++ ReactionWheelWizard/ReactionWheelWizard.ino
@@ -48,7 +48,7 @@ void loop() {
   Serial.println("Hello world!");
 
   if (uart.getFWversion(PITCH_CAN_ID)) {
-    printf("fw canId2: %d", uart.fw_version.major);
+    printf("fw canId2: %d %d", uart.fw_version.major, uart.fw_version.minor);
   }
 
   sensors_event_t orientationData, angVelocityData;
