1b3f8138bde85aaa2512a3de99b6dc0ffac4caab
Cameron Bannasch <cameron.bannasch@gmail.com>
2026-07-13 21:03:29 -0700

    Add nl

--- ReactionWheelWizard/ReactionWheelWizard.ino
+++ ReactionWheelWizard/ReactionWheelWizard.ino
@@ -73,8 +73,8 @@ void loop() {
   float roll_command = roll_error * KP_ROLL - d_roll_dt * KD_ROLL;
   float pitch_command = pitch_error * KP_PITCH - d_pitch_dt * KD_PITCH;
 
-  Serial.printf("ROLL -> err: %d deg command: %d amps setpoint: %d deg", roll_error, roll_command, DESIRED_ROLL);
-  Serial.printf("PITCH -> err: %d deg command: %d amps setpoint: %d deg", pitch_error, pitch_command, DESIRED_PITCH);
+  Serial.printf("ROLL -> err: %d deg command: %d amps setpoint: %d deg\n", roll_error, roll_command, DESIRED_ROLL);
+  Serial.printf("PITCH -> err: %d deg command: %d amps setpoint: %d deg\n", pitch_error, pitch_command, DESIRED_PITCH);
 
   if (INVERT_PITCH) {
     pitch_command = -pitch_command;
